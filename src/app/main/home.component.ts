f61d3124dd99ada99150b9091949c12a002a9f71
BenHuang <jscode1972@gmail.com>
2022-12-29 04:24:43 +0800

    2

--- src/app/main/home.component.ts
+++ src/app/main/home.component.ts
@@ -14,25 +14,10 @@ import { Component } from '@angular/core';
       <br/>
       <a target="_blank" href="https://stackblitz.com/edit/ng-book-router">ng-book-router</a> <br/>
       <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-ngswitch-directive"> ngswitch-directive </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-componet-outlet"> component-outlet </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-view-child-ngif"> view-child-ngif </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-attribute-directive-style"> attribute-directive-style </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-directive-export-as"> directive-export-as </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-attribute-directive-confirm"> attribute-directive-confirm </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-structural-directive"> structural-directive </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-host-listener-decorator"> host-listener-decorator </a> <br/>
-      <a target="_blank" href="https://stackblitz.com/edit/ng-book-host-binding-decorator"> host-binding-decorator </a> <br/>
-      <br/>
-
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
-
       
+      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
+      <a target="_blank" href="xxxxx"> xxxx </a> <br/>
+
       </p>
   `,
   styles: [
