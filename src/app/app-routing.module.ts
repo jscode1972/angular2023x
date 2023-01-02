27ed314e5dc72fc43645433adc74a00c150fd2fe
BenHuang <jscode1972@gmail.com>
2023-01-03 03:40:23 +0800

    demo page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
 //
 import { WeblinkComponent } from './weblink/weblink.component';
 
 const routes: Routes = [
-  //注意! home 有自己的 routing, 故以下一行拿掉
+  //注意! home 有自己的 routing, 故以下一行拿掉 (home/contact/about)
   //{ path: '', component: HomeComponent },
   //{ path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'weblink', component: WeblinkComponent },   // 網路連結
+  { path: 'demo', component: DemoComponent }
   //{ path: 'xyz', children: [
   //  { path: '', component: XyzComponent },
   //  { path: 'x1', component: X1Component },
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  styleUrls: [
+   './demo.component.css']
 })
 export class DemoComponent {
 
